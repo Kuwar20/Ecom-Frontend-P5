3105c8d03b2775a45c74b44b1be499131d302382
Kuwar20 <kuwarx1@gmail.com>
2024-01-08 23:41:06 +0530

    Update project structure and add new pages

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Cart from "./pages/Cart";
 
 function App() {
 
   return (
-    <>
-      <h1>Hello react</h1>
-    </>
+    <Router>
+      {/* <Header /> */}
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/cart" element={<Cart />} />
+
+      </Routes>
+    </Router>
   )
 }
 
