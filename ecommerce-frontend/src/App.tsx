ba1a9e6729810861b8189bccca724b5cff7e49bb
Kuwar20 <kuwarx1@gmail.com>
2024-01-09 18:38:31 +0530

    Add Header component

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { lazy, Suspense } from "react";
 import Loader from "./components/Loader";
+import Header from "./components/Header";
 const Home = lazy(() => import("./pages/Home"));
 const Search = lazy(() => import("./pages/Search"));
 const Cart = lazy(() => import("./pages/Cart"));
@@ -31,6 +32,8 @@ function App() {
   return (
     <Router>
       {/* <Header /> */}
+      <Header />
+      
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
