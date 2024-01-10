76fbbe413bac33e23cfc9df0c5b48ee715a68e0f
Kuwar20 <kuwarx1@gmail.com>
2024-01-10 14:50:22 +0530

    Add product card and link to home page

--- ecommerce-frontend/src/pages/Home.tsx
+++ ecommerce-frontend/src/pages/Home.tsx
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
+import ProductCard from "../components/Product-Card";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const addToCartHandler = () => {};
 
-export default Home
+  return (
+    <div className="home">
+      <section></section>
+
+      <h1>
+        Latest Product
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
+      <main>
+        <ProductCard productId="kuwar" name="singh" price={422} stock={435} handler={addToCartHandler} photo="https://m.media-amazon.com/images/I/618d5bS2lUL._SX466_.jpg" />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
