0bb63827f5ad5d11d29486e4192a8da43f645f24
Kuwar20 <kuwarx1@gmail.com>
2024-01-12 16:50:27 +0530

    Add search functionality and filters to the search page

--- ecommerce-frontend/src/pages/Search.tsx
+++ ecommerce-frontend/src/pages/Search.tsx
@@ -1,7 +1,90 @@
-const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+import { useState } from "react";
+import ProductCard from "../components/Product-Card";
 
-export default Search
+const Search = () => {
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [maxPrice, setMaxPrice] = useState(100000);
+  const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
+
+  const addToCartHandler = () => { };
+
+  const isPrevPage = true;
+  const isNextPage = true;
+
+  return (
+    <div className="product-search-page">
+      <aside>
+        <h2>Filters</h2>
+        <div>
+          <h4>Sort</h4>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="">None</option>
+            <option value="asc">Price (Low to High)</option>
+            <option value="dsc">Price (High to Low)</option>
+          </select>
+        </div>
+        <div>
+          <h4>Max Price:{maxPrice || ""}</h4>
+          <input
+            type="range"
+            min={100}
+            max={100000}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(Number(e.target.value))}
+          ></input>
+        </div>
+        <div>
+          <h4>Category</h4>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="">Sample1</option>
+            <option value="">Sample2</option>
+          </select>
+        </div>
+      </aside>
+      <main>
+        <h1>Products</h1>
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="search-product-list">
+          <ProductCard
+            productId="kuwar"
+            name="singh"
+            price={422}
+            stock={435}
+            handler={addToCartHandler}
+            photo="https://m.media-amazon.com/images/I/618d5bS2lUL._SX466_.jpg"
+          />
+        </div>
+        <article>
+          <button
+            disabled={!isPrevPage}
+            onClick={() => setPage((prev) => prev - 1)}
+          >
+            Prev
+          </button>
+          <span>
+            {page} of {4}
+          </span>
+          <button
+            disabled={!isNextPage}
+            onClick={() => setPage((prev) => prev + 1)}
+          >
+            Next
+          </button>
+        </article>
+      </main>
+    </div>
+  );
+};
+
+export default Search;
